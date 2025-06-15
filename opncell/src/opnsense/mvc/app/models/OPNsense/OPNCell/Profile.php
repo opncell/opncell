659609980f3d324590642fbd70c18168bb481a0e
Robert Hamblet <rob@hamblet.com>
2025-06-15 18:12:44 -0500

    Update Profile.php

--- opncell/src/opnsense/mvc/app/models/OPNsense/OPNCell/Profile.php
+++ opncell/src/opnsense/mvc/app/models/OPNsense/OPNCell/Profile.php
@@ -1,8 +1,9 @@
 <?php
 
 /*
-    Copyright (C) 2023 Digital Solutions <support@ds.co.ug
+    Copyright (C) 2023 Digital Solutions <support@ds.co.ug>
     Copyright (C) 2023 Primrose Namirimu <primrose@ds.co.ug>
+    Copyright (C) 2025 Wireless Laboratories <rob.hamblet@wire-labs.com>
     All rights reserved.
     Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions are met:
